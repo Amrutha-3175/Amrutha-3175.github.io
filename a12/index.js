b8f02cf9a00dc534ca398666e8f90ede6961978b
Amrutha-3175 <23r11a0543@gcet.edu.in>
2025-05-16 19:30:09 +0530

    updated

---
+++ a12/index.js
@@ -0,0 +1,6 @@
+const products = [
+    {id: 1, name : "Apple", price: 50},
+    { id: 2, name:"Banana", price: 25},
+    {id: 3, name:"Orange", price:40}
+]
+products.forEach((x)=>console.log(x.name));
