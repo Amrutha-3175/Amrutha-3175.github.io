1d5e497ba660cb1171fbbca1f62777a952ee3ca0
Amrutha-3175 <23r11a0543@gcet.edu.in>
2025-05-17 18:48:34 +0530

    updated

---
+++ a14/index.js
@@ -0,0 +1,9 @@
+const items = [
+  { name: 'Apple', price: 50, status:'pending' },
+  { name: 'Banana', price: 30, status:'pending' },
+  { name: 'Orange', price: 80, status:'pending' }
+];
+const updatedItems = items.map((item)=>({...item,price:item.price+5,status:completed}));
+updatedItems.forEach(item => {
+  console.log(`Name: ${item.name}, Price: ${item.price},Status: ${item.status}`);
+});
