81712f1d098ef399853b4ee9087d7aa3e04ca0ac
Amrutha-3175 <23r11a0543@gcet.edu.in>
2025-05-19 20:06:09 +0530

    updated

--- p4/index.js
+++ p4/index.js
@@ -1,4 +1,36 @@
+const cardInput = document.getElementById("cardNumber");
+const pinInput = document.getElementById("pin");
+const submitBtn = document.getElementById("submit");
+const loginForm = document.getElementById("loginForm");
+const welcomeScreen = document.getElementById("welcomeScreen");
+
+
+
+
 const customers = [
-  { cart: "1234567890", pin: "1234", name: "John", balance: 0 },
-  { cart: "1234567891", pin: "2345", name: "Cathy", balance: 0 },
+  { cardNumber: "1234567890", pin: "1234", name: "John", balance: 0 },
+  { car: "1234567891", pin: "2345", name: "Cathy", balance: 0 },
 ];
+function validateInputs() {
+  const card = cardInput.value.trim();
+  const pin = pinInput.value.trim();
+  const user = validUsers[card];
+
+  if (user && user.pin === pin) {
+    submit.disabled = false;
+  } else {
+    submit.disabled = true;
+  }
+}
+
+cardInput.addEventListener("input", validateInputs);
+pinInput.addEventListener("input", validateInputs);
+
+submitBtn.addEventListener("click", () => {
+  const card = cardInput.value.trim();
+  const user = validUsers[card];
+
+  loginForm.style.display = "none";
+  welcomeScreen.style.display = "block";
+  welcomeScreen.textContent = `Welcome, ${user.name}!`;
+});
